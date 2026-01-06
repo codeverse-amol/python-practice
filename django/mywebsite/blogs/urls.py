46ddfc6bf4a9b35dfcc04e30fbc48fb8eceab489
Amol Bandgar <amolbs2109@gmail.com>
2026-01-06 20:54:52 +0530

    Delete django/mywebsite directory

--- django/mywebsite/blogs/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-
-urlpatterns = [
-    path("", views.home),
-    path("allposts", views.blogpost),
-    path("allposts/<slug:blog>", views.blog_post)
-]
